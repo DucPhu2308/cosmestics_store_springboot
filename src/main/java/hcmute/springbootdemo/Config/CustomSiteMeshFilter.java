3b0dea6f7ad8b887cf15903654141e5655527a18
DucPhu2308 <nguyenducphu2308@gmail.com>
2023-10-31 22:47:01 +0700

    first commit

---
+++ src/main/java/hcmute/springbootdemo/Config/CustomSiteMeshFilter.java
@@ -0,0 +1,13 @@
+package hcmute.springbootdemo.Config;
+
+import org.sitemesh.builder.SiteMeshFilterBuilder;
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
+
+public class CustomSiteMeshFilter extends ConfigurableSiteMeshFilter {
+    @Override
+    protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
+        builder
+        .addDecoratorPath("/*","/decorators/web.jsp")
+        .addDecoratorPath("/admin/*","/decorators/admin.jsp");
+    }
+}
